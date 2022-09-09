b69c2c7bf4488ac474ab48851c9add8e473c7188
anastasia <anastasia.miliutina@gmail.com>
2022-09-09 17:12:39 +0300

    homework

---
+++ src/main/java/Cell.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cell {
+    List<Cell> near;
+    Status status;
+
+    public Cell(){
+        status = Status.NONE;
+        near = new ArrayList<>();
+    }
+    void addNear(Cell cell){
+        near.add(cell);
+    }
+    void step1(){
+        int around = countNearCells();
+        status = status.step1(around);
+    }
+
+    void step2(){
+        status = status.step2();
+    }
+
+    int countNearCells(){
+        int count = 0;
+        for ( Cell cell : near)
+          if (cell.status.isCell())
+              count++;
+        return count;
+    }
+
+
+}
